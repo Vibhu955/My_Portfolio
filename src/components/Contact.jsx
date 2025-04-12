4ad51160ef5da9a0bc7060c8ef53b1942cdf50fa
Vibhu955 <vibhu.us24@gmail.com>
2025-04-12 22:39:46 +0530

    ..

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,18 @@
-import { div } from "framer-motion/client";
+import React from 'react'
 import { useState } from "react";
 
-function VerticalTimeline() {
-return (
-  <div>Contact  </div>
-)
+function Contact() {
+  return (
+    <div className="contact-section" id="contact">
+      <div>
+        <div className="contact-heading">
+          <h2>Contact Me</h2>
+        </div>
+        <div className="contact-on">
+          <div></div>
+        </div>
+      </div>
+    </div>
+  )
 }
-export default VerticalTimeline;
+export default Contact;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,74 +1,3 @@
-// import React, { useEffect, useRef } from "react";
-// import "./style.css"; // Assuming you want to apply custom styles here
-// import { motion } from "framer-motion";
-
-// const cardDetails = {
-//   title: ['Portfolio', 'Health Check Up', 'File Sharing System'],
-//   text: ['Explore my work—ranging from ML-powered apps to full-stack web platforms and smart IoT integrations.',
-//     'A smart ML-based platform that monitors patient vitals in real-time and sends data to the Hospital Management & Maintainance.',
-//     'A secure, high-performance system with support for concurrent uploads, metadata caching, and scalable cloud/local storage.'],
-//   tech: ['React-Vite, HTML5, CSS, ThreeFiber', 'React, Express, MongoDB, Node.js, Python, TensorFlow ', 'PostgreSQL, Node.js, Express, React, GOlang, Redis, S3'],
-//   link: ['https://github.com/Vibhu955/My_Portfolio', 'https://github.com/Vibhu955/HealthCheck_', 'https://github.com/Vibhu955/file_sharing_system']
-// }
-
-// function ProjectCards({ index }) {
-
-//   return (
-
-//     <div key={index} className="card" style={{ width: "18rem" }}>
-//       <img src="..." className="card-img-top" alt="..." />
-//       <div className="card-body">
-//         <h5 className="card-title">{cardDetails.title[index]}</h5>
-//         <p className="card-text">{cardDetails.text[index]}</p>
-//         <p className="card-text"><small className="text-muted"><strong>Tech Stack: {cardDetails.tech[index]}</strong></small></p>
-//         <a href={cardDetails.link[index]} className="btn btn-primary">View More <i class="fa-brands fa-github"></i></a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// function Projects() {
-//   return (
-//     <div style={{
-//       backgroundColor: "#0f0b0b",
-//       color: "rgba(245, 241, 241, 0.926)",
-//       width: "100vw",
-//       minHeight: "100vh",
-//       padding: "3rem 0",
-//       textAlign: "center"
-//     }}>
-//       {/* Title Section */}
-//       <motion.div
-//         style={{ marginTop: "7rem" }}
-//         initial={{ y: 250, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//         viewport={{ once: true }}
-//         transition={{ duration: 2 }}
-//       >
-//         <h2 style={{ fontSize: "2rem", fontWeight: "bold", marginBottom: "1rem" }}>My Projects</h2>
-//       </motion.div>
-//       {/* Divider Section */}
-//       <motion.div
-//         style={{ marginBottom: "2rem" }}
-//         className="timeline-divider"
-//         initial={{ width: 0, opacity: 0 }}
-//         animate={{ width: 50, opacity: 1 }}
-//         transition={{ duration: 1.5 }}
-//       ></motion.div>
-
-//       {/* Project Cards Section */}
-//       <div style={{ display: "flex", justifyContent: "center", gap: "2rem", flexWrap: "wrap" }}>
-//         {cardDetails.title.map((_, index) => (
-//           <ProjectCards index={index} />
-//         ))}
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default Projects;
-
 import React from "react";
 import "./style.css";
 import { motion } from "framer-motion";
@@ -94,7 +23,7 @@ const cardDetails = {
 
 function ProjectCards({ index }) {
   return (
-    <div className="custom-card">
+    <div className="projcard">
       <div className="card-content">
         <h3>{cardDetails.title[index]}</h3>
         <p>{cardDetails.text[index]}</p>
