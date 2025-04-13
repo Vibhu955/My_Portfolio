1d229ed879da7babe75e1ed98cfd1c526143cd60
Vibhu955 <vibhu.us24@gmail.com>
2025-04-14 03:00:16 +0530

    ..

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,76 +1,5 @@
-// import React from 'react';
-// import { motion } from 'framer-motion';
-
-// function Contact() {
-//   return (
-//     <div className="contact-section" id="contact">
-//       {/* Title */}
-//       <motion.div
-//         className="contact-heading"
-//         initial={{ y: -100, opacity: 0 }}
-//         whileInView={{ y: 0, opacity: 1 }}
-//         transition={{ duration: 1 }}
-//       >
-//         <h2>Contact Me</h2>
-//       </motion.div>
-//       <motion.div
-//         style={{ marginBottom: "2rem" }}
-//         className="timeline-divider"
-//         initial={{ width: 0, opacity: 0 }}
-//         animate={{ width: 50, opacity: 1 }}
-//         transition={{ duration: 1.5 }}
-//       ></motion.div>
-
-//       {/* Contact Info  */}
-//       <div className="contact-info">
-//         <motion.div
-//           className="contact-details"
-//           initial={{ x: -100, opacity: 0 }}
-//           whileInView={{ x: 0, opacity: 1 }}
-//           transition={{ duration: 1 }}
-//         >
-//           <p><strong>Name:</strong> Vibhuti Joshi</p>
-//           <div className="mail">
-//             <p><strong>Email:</strong></p>
-//             <a href="mailto:vibhu.us24@gmail.com" target="_blank" rel="noopener noreferrer">
-//               vibhu.us24@gmail.com
-//             </a>
-//           </div>
-//           <div className="mail">
-//             <p><strong>Official Email:</strong></p>
-//             <a href="mailto:vibhuti.2022@vitstudent.ac.in" target="_blank" rel="noopener noreferrer">
-//               vibhuti.2022@vitstudent.ac.in
-//             </a>
-//           </div>
-//         </motion.div>
-
-//         <motion.div
-//           className="contact-icons"
-//           initial={{ x: 100, opacity: 0 }}
-//           whileInView={{ x: 0, opacity: 1 }}
-//           transition={{ duration: 1 }}
-//         >
-//           <a href="https://www.instagram.com/vibhu955/" target="_blank" rel="noopener noreferrer">
-//             <i className="fab fa-instagram fa-xl" title='Instagram'></i>
-//           </a>
-//           <a href="https://wa.me/918800000000" target="_blank" rel="noopener noreferrer">
-//             <i className="fab fa-whatsapp fa-xl" title='WhatsApp'></i>
-//           </a>
-//           <a href="https://www.linkedin.com/in/vibhu955/" target="_blank" rel="noopener noreferrer">
-//             <i className="fab fa-linkedin fa-xl" title='LinkedIn'></i>
-//           </a>
-//           <a href="https://github.com/vibhu955" target="_blank" rel="noopener noreferrer">
-//             <i className="fab fa-github fa-xl" title='Github'></i>
-//           </a>
-//         </motion.div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Contact;
 import React, { useEffect, useRef, useState } from 'react';
-import { motion, px } from 'framer-motion';
+import { motion} from 'framer-motion';
 import * as THREE from 'three';
 import WAVES from 'vanta/dist/vanta.waves.min'; // You can change to NET, FOG, etc.
 
@@ -92,7 +21,7 @@ function Contact() {
           Width: 50.0,
           scale: 1.0,
           scaleMobile: 1.0,
-          color: 0x000, // customize wave color
+          color: 0x000, 
           shininess: 50,
           waveHeight: 40,
           waveSpeed: 0.5,
@@ -152,16 +81,16 @@ function Contact() {
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ duration: 1 }}
           >
-            <a href="https://www.instagram.com/vibhu955/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/vibhuti_love7/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-instagram fa-xl" title='Instagram'></i>
             </a>
-            <a href="https://wa.me/918800000000" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/9552704155" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-whatsapp fa-xl" title='WhatsApp'></i>
             </a>
-            <a href="https://www.linkedin.com/in/vibhu955/" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/vibhuti-joshi-805a1a274/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-linkedin fa-xl" title='LinkedIn'></i>
             </a>
-            <a href="https://github.com/vibhu955" target="_blank" rel="noopener noreferrer">
+            <a href="https://github.com/Vibhu955/" target="_blank" rel="noopener noreferrer">
               <i className="fab fa-github fa-xl" title='Github'></i>
             </a>
           </motion.div>
